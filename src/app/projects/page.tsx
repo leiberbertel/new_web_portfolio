d38adaf2fea9fcc1ce9b6b650f2d774a7ca1545c
leiber bertel <leiberbertel777@gmail.com>
2025-01-04 00:40:47 -0500

    refactor: extracts code from project images, creates stand-alone carousel component and updates integration in Projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
+import Carousel from "@/components/Carousel";
 
 function Projects() {
   return (
@@ -46,138 +47,7 @@ function Projects() {
             </p>
           </div>
         </section>
-        <section className="md:flex hidden flex-col items-center gap-10">
-          <h2 className="acorn-bold text-6xl max-w-[600px] text-center">
-            Finished
-          </h2>
-          <div className="flex justify-between w-full">
-            <div
-              style={{
-                background: "linear-gradient(100deg, #FAAC12, #E07262)",
-              }}
-              className="w-[35%] relative h-[500px] rounded-[40px] overflow-hidden"
-            >
-              <h1 className=" py-5 z-10 px-10 right-0 absolute acorn-bold text-4xl">
-                Interactive Financial Dashboard
-              </h1>
-              <a href={"https://github.com/leiberbertel/financial_dashboard_dash"} target="_blank">
-                <div
-                  style={{
-                    width: "500px",
-                    height: "360px",
-                    position: "absolute",
-                    bottom: 0,
-                    right: 0,
-                  }}
-                >
-                  <Image
-                    className="object-cover h-auto absolute translate-x-[200px] bottom-0 rounded-[40px] hover:translate-x-[180px] transition-all"
-                    src="/dashboard_financial.png"
-                    layout="fill"
-                    alt="dashboard financial"
-                  />
-                </div>
-              </a>
-            </div>
-            <div
-              style={{
-                background: "linear-gradient(100deg, #7EBAFF, #2400FF)",
-              }}
-              className="w-[60%] relative h-[500px] rounded-[40px] overflow-hidden"
-            >
-              <h1 className=" py-5 z-10 px-10 right-0 absolute acorn-bold text-4xl">
-                Task Planner
-              </h1>
-              <a
-                href={"https://todoapp-12a65.web.app/"}
-                target="_blank"
-              >
-                <div
-                  style={{
-                    width: "500px",
-                    height: "400px",
-                    position: "absolute",
-                    bottom: 0,
-                    right: 0,
-                  }}
-                >
-                  <Image
-                    className="object-cover h-auto absolute translate-x-[50px] bottom-0 rounded-[40px] hover:translate-x-[20px] transition-all"
-                    src="/task_planner.png"
-                    layout="fill"
-                    alt="task planner"
-                  />
-                </div>
-              </a>
-            </div>
-          </div>
-        </section>
-        <section className="flex md:hidden flex-col items-center gap-10">
-          <h2 className="acorn-bold text-6xl max-w-[600px] text-center">
-            Finished
-          </h2>
-          <div className="flex flex-col justify-between w-full gap-16">
-            <div
-              style={{
-                background: "linear-gradient(100deg, #FAAC12, #E07262)",
-              }}
-              className="w-[100%] relative h-[500px] rounded-[40px] overflow-hidden"
-            >
-              <h1 className=" py-5 z-10 px-10 right-0 text-center acorn-bold text-4xl">
-                Interactive Financial Dashboard
-              </h1>
-              <a href={"https://github.com/leiberbertel/financial_dashboard_dash"} target="_blank">
-                <div
-                  style={{
-                    width: "500px",
-                    height: "360px",
-                    position: "absolute",
-                    bottom: 0,
-                    right: 0,
-                  }}
-                >
-                  <Image
-                    className="object-cover h-auto absolute translate-x-[230px] bottom-0 rounded-[40px] hover:translate-x-[180px] transition-all"
-                    src="/dashboard_financial.png"
-                    layout="fill"
-                    alt="dashboard financial"
-                  />
-                </div>
-              </a>
-            </div>
-            <div
-              style={{
-                background: "linear-gradient(100deg, #7EBAFF, #2400FF)",
-              }}
-              className="w-[100%] relative h-[500px] rounded-[40px] overflow-hidden"
-            >
-              <h1 className=" py-5 z-10 px-10 right-0 text-center acorn-bold text-4xl">
-                Task Planner
-              </h1>
-              <a
-                href={"https://todoapp-12a65.web.app/"}
-                target="_blank"
-              >
-                <div
-                  style={{
-                    width: "500px",
-                    height: "400px",
-                    position: "absolute",
-                    bottom: 0,
-                    right: 0,
-                  }}
-                >
-                  <Image
-                    className="object-cover h-auto absolute translate-x-[230px] bottom-0 rounded-[40px] hover:translate-x-[20px] transition-all"
-                    src="/task_planner.png"
-                    layout="fill"
-                    alt="task planner"
-                  />
-                </div>
-              </a>
-            </div>
-          </div>
-        </section>
+        <Carousel />
         <Footer />
       </main>
     </>
